69b7a07269e59660aac2b83f61e883aae46c8d78
iacore <noreply+gpg-stub@1a-insec.net>
2024-04-20 11:45:59 +0000

    init

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	let name = ['existing option'];
+</script>
+
+<h1>Refresh to reset</h1>
+<h1>{JSON.stringify(name)}</h1>
+Try click on these:
+<input type="checkbox" value="no" bind:group={name}>
+<input type="checkbox" value="yes" bind:group={name}>
